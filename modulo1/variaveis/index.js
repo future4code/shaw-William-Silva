f84fe95032c36352cabdf4e8b6fbbb01644917ff
William <william-freitas18@hotmail.com>
2022-02-14 17:11:39 -0300

    Exercício de variáveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,35 @@
+/*Exercícios de interpretação de código
+
+1.
+    10
+    10 5
+
+primeiro definiu duas variáveis let sendo a=10 e b=10
+em seguida usou console.log(b) para imprimir o valor de b(10)
+depois mudou o valor da variável de b para 5
+terminando imprimindo o valor de a(10) e de b(5)
+
+2.
+    10 10 10
+
+primeiro definiu duas variáveis let sendo a=10 e b=20
+em seguida definiu valores para c=a, b=c e a=b se tornando assim c=10, b=10 e a=10
+e por último imprimiu o valor de a(10), b(10) e c(10)
+
+3. 
+ 
+o programa calcula o quanto o usuário recebe por hora calculando as variáveis de p e t.
+
+variável p muda para cargaHoraria
+variável t muda para ganhoPorDia */
+
+
+let nome = prompt("Digite seu nome")
+let idade = prompt ("Digite a sua idade")
+idade = 23
+
+console.log("Tipo de nome", typeof nome)
+console.log("Tipo de idade", typeof idade)
+
+/* ambos ficaram foram impresso como "string" porque eu não atribui nenhum outro tipo específico de variáveis*/
+
