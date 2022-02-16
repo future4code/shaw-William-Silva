24b1f4c920375bfdbe024aebe27d30724b188752
William <william-freitas18@hotmail.com>
2022-02-16 15:43:05 -0300

    Exercício da aula de arrays

---
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,71 @@
+//EXERCÍCIOS DE INTERPRETAÇÃO
+
+/* 1.
+a. undefined
+b. null
+c. 11
+d. 3
+e. [3,19,5,6,7,8,9,10,11,12,13]
+f. 9 */
+
+/* 2.
+
+SUBI NUM ÔNIBUS EM MIRROCOS 27 */
+
+// EXERCÍCIOS DE ESCRITA
+
+//1.
+
+/* let nome = prompt("Digite seu nome!")
+let email = prompt("Digite seu email!")
+emailDoUsuario = email
+nomeDoUsuario = nome
+
+console.log("O e-mail",`${emailDoUsuario}`,"foi cadastrado com sucesso. Seja bem-vinda(o)",`${nomeDoUsuario}`,"!") */
+
+//2.
+//a. 
+
+/* let comidasFavoritas = ['Pizza','Bolo','Coxinha','Paella','Lasanha']
+console.log(comidasFavoritas) */
+
+//b.
+
+/* let comidasFavoritas = ['Pizza','Bolo','Coxinha','Paella','Lasanha']
+console.log(comidasFavoritas)
+
+primeiraComida = comidasFavoritas[0]
+segundaComida = comidasFavoritas[1]
+terceiraComida = comidasFavoritas[2]
+quartaComida = comidasFavoritas[3]
+quintaComida = comidasFavoritas[4]
+
+console.log("Essas são as minhas comidas preferidas:")
+console.log(primeiraComida)
+console.log(segundaComida)
+console.log(terceiraComida)
+console.log(quartaComida)
+console.log(quintaComida) */
+
+//c.
+
+/* let qualComidaFavorita = prompt("Qual a sua comida favorita?")
+let comidasFavoritas = ['Pizza','Bolo','Coxinha','Paella','Lasanha']
+comidaFavoritaCliente = qualComidaFavorita
+let novaComidasFavoritas = ['Pizza', `${comidaFavoritaCliente}` ,'Coxinha','Paella','Lasanha']
+
+console.log(novaComidasFavoritas) */
+
+//3.
+
+/* let listaDeTarefas = []
+
+listaDeTarefas = [prompt("Tarefa realizada 1:"), prompt("Tarefa realizada 2:"), prompt("Tarefa realizada 3: ")]
+
+console.log(listaDeTarefas)
+
+let tarefaRealizada = prompt("Digite um número de 0 a 2 correspondente a tarefa já realizada")
+
+listaDeTarefas.splice(tarefaRealizada, 1)
+
+console.log(listaDeTarefas) */
