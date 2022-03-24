238ae4d61606e773bc6a0e14bd5363ceeb23000a
William <william-freitas18@hotmail.com>
2022-03-24 13:54:59 -0300

    Exercício de Componentes de Classe: Atualização

--- modulo2/estados-insta4/insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/insta4/src/App.js
@@ -1,36 +1,27 @@
 import React from 'react';
-import styled from 'styled-components'
+import './App.css';
 import Post from './components/Post/Post';
-
-const MainContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-`
 
 class App extends React.Component {
   render() {
     return (
-      <MainContainer>
+      <div className={'app-container'}>
         <Post
           nomeUsuario={'paulinha'}
-          fotoUsuario={`https://picsum.photos/50/50`}
-          fotoPost={`https://picsum.photos/200/150`}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
         />
-
         <Post
           nomeUsuario={'gabriel'}
-          fotoUsuario={`https://picsum.photos/50/60`}
-          fotoPost={`https://picsum.photos/200/160`}
+          fotoUsuario={'https://picsum.photos/50/51'}
+          fotoPost={'https://picsum.photos/200/151'}
         />
-
         <Post
           nomeUsuario={'laura'}
-          fotoUsuario={`https://picsum.photos/50/70`}
-          fotoPost={`https://picsum.photos/200/170`}
+          fotoUsuario={'https://picsum.photos/50/52'}
+          fotoPost={'https://picsum.photos/200/152'}
         />
-      </MainContainer>
+      </div>
     );
   }
 }
--- modulo2/estados-insta4/insta4/insta4/src/components/Post/Post.js
+++ modulo2/estados-insta4/insta4/insta4/src/components/Post/Post.js
@@ -1,55 +1,34 @@
 import React from 'react'
-import styled from 'styled-components'
+import './Post.css'
 
-import { IconeComContador } from '../IconeComContador/IconeComContador'
+import {IconeComContador} from '../IconeComContador/IconeComContador'
 
 import iconeCoracaoBranco from '../../img/favorite-white.svg'
 import iconeCoracaoPreto from '../../img/favorite.svg'
 import iconeComentario from '../../img/comment_icon.svg'
-import { SecaoComentario } from '../SecaoComentario/SecaoComentario'
-
-const PostContainer = styled.div`
-  border: 1px solid gray;
-  width: 300px;
-  margin-bottom: 10px;
-`
-
-const PostHeader = styled.div`
-  height: 40px;
-  display: flex;
-  align-items: center;
-  padding-left: 10px;
-`
-
-const PostFooter = styled.div`
-  height: 40px;
-  display: flex;
-  align-items: center;
-  padding: 0 10px;
-  justify-content: space-between;
-`
-
-const UserPhoto = styled.img`
-  height: 30px;
-  width: 30px;
-  margin-right: 10px;
-  border-radius: 50%;
-`
-
-const PostPhoto = styled.img`
-  width: 100%;
-`
+import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
 
 class Post extends React.Component {
   state = {
-    curtido: true,
-    numeroCurtidas: 10,
-    comentando: true,
-    numeroComentarios: 10
+    curtido: false,
+    numeroCurtidas: 0,
+    comentando: false,
+    numeroComentarios: 0
   }
 
   onClickCurtida = () => {
-    console.log('Curtiu!')
+    let novoNumeroCurtidas
+
+    if(this.state.curtido) {
+      novoNumeroCurtidas = this.state.numeroCurtidas - 1
+    } else {
+      novoNumeroCurtidas = this.state.numeroCurtidas + 1
+    }
+
+    this.setState({
+      curtido: !this.state.curtido,
+      numeroCurtidas: novoNumeroCurtidas
+    })
   }
 
   onClickComentario = () => {
@@ -68,7 +47,7 @@ class Post extends React.Component {
   render() {
     let iconeCurtida
 
-    if (this.state.curtido) {
+    if(this.state.curtido) {
       iconeCurtida = iconeCoracaoPreto
     } else {
       iconeCurtida = iconeCoracaoBranco
@@ -76,33 +55,33 @@ class Post extends React.Component {
 
     let componenteComentario
 
-    if (this.state.comentando) {
-      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario} />
+    if(this.state.comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
     }
 
-    return <PostContainer>
-      <PostHeader>
-        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'} />
+    return <div className={'post-container'}>
+      <div className={'post-header'}>
+        <img className={'user-photo'} src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
         <p>{this.props.nomeUsuario}</p>
-      </PostHeader>
+      </div>
 
-      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'} />
+      <img className={'post-photo'} src={this.props.fotoPost} alt={'Imagem do post'}/>
 
-      <PostFooter>
+      <div className={'post-footer'}>
         <IconeComContador
           icone={iconeCurtida}
           onClickIcone={this.onClickCurtida}
           valorContador={this.state.numeroCurtidas}
         />
 
-          <IconeComContador
-            icone={iconeComentario}
-            onClickIcone={this.onClickComentario}
-            valorContador={this.state.numeroComentarios}
-          />
-        </PostFooter>
-        {componenteComentario}
-    </PostContainer>
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={this.onClickComentario}
+          valorContador={this.state.numeroComentarios}
+        />
+      </div>
+      {componenteComentario}
+    </div>
   }
 }
 
