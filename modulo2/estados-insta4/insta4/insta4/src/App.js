13bb13f983b66c20ca5f2f57caa5f727756bd2b4
William <william-freitas18@hotmail.com>
2022-03-24 16:34:37 -0300

    Exercício de Componentes de Classe: Atualização

--- modulo2/estados-insta4/insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/insta4/src/App.js
@@ -3,7 +3,40 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+  state = {
+    id: 0,
+    post: "",
+    imagem: "",
+    postagem: [
+      {
+        nome: "paulinha",
+        fotoUsuario: "https://picsum.photos/50/50",
+        fotoPost: "https://picsum.photos/200/150"
+      },
+      {
+        nome: "gabriel",
+        fotoUsuario: "https://picsum.photos/50/51",
+        fotoPost: "https://picsum.photos/200/151"
+      },
+      {
+        nome: "laura",
+        fotoUsuario: "https://picsum.photos/50/52",
+        fotoPost: "https://picsum.photos/200/152"
+      }
+    ]
+  };
+
   render() {
+    const renderizaNaTela = this.state.postagem.map((postagem) => {
+      return (
+        <Post
+          nome={postagem.nome}
+          imagemDoUsuario={postagem.fotoUsuario}
+          imagemDoPost={postagem.fotoPost}
+          funcaoDeDeletar={this.DeletePost}
+        />
+      );
+    });
     return (
       <div className={'app-container'}>
         <Post
