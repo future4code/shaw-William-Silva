e37d48e3a16f65b06b4ddce158ffd1a836fe44d0
William <william-freitas18@hotmail.com>
2022-03-27 18:34:05 -0300

    Projeto de Fixação: WhatsLab - Atualização

--- modulo2/projeto-whatslab/projeto-whatslab/src/App.js
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.js
@@ -1,34 +1,67 @@
 import './App.css';
+import React from "react"
 
-const pessoas = [
-  {
-    nome: 'Wiliam'
-  },
+class App extends React.Component {
+  state = {
+    nome: "",
+    mensagem: "",
+  };
 
-  {
-    nome: 'Gabriel'
-  },
+  onChangeNome = (event) => {
+    this.setState({ nome: event.target.value });
+  };
 
-  {
-    nome: 'Paula'
+  onChangeMensagem = (event) => {
+    this.setState({ mensagem: event.target.value });
+  };
+
+  enviarInputs = () => {
+    console.log("nome:", this.state.nome);
+    console.log("mensagem:", this.state.mensagem);
+    this.setState({ nome: "", mensagem: "" });
+  };
+
+  onClickMensagem = () => {
+    const novaMensagem = {
+      nome: this.state.nome,
+      mensagem: this.state.mensagem,
+    };
+
+    const novoArray = [...this.state.postagem];
+    novoArray.push(novaMensagem);
+
+    this.setState({ mensagem: novoArray });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <div className='caixa-whatslab' >
+          
+        </div>
+
+        <div className='caixa-input'>
+          <input
+            id='usuario'
+            type="text"
+            placeholder='Usuário'
+            onChange={this.onChangeNome}
+            value={this.state.nome}
+          />
+
+          <input
+            id='mensagem'
+            type="text"
+            placeholder='Mensagem'
+            onChange={this.onChangeMensagem}
+            value={this.state.mensagem}
+          />
+
+          <button id='enviar' onClick={this.enviarInputs}>Enviar</button>
+        </div>
+      </div >
+    );
   }
-]
-
-
-function App() {
-  return (
-    <div className="App">
-      <div className="caixa-whatslab" >
-
-      </div>
-
-      <div className="caixa-input">
-        <input type='text' name='usuario' id='usuario' />
-        <input type='text' name='mensagem' id='mensagem' />
-        <input type='submit' name='enviar' id='enviar' />
-      </div>
-    </div>
-  );
 }
 
 export default App;
