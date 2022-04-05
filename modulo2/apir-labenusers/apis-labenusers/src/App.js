2ce05d99d1d73124a4664ee865ed197be4bcaa09
William <william-freitas18@hotmail.com>
2022-04-05 16:51:44 -0300

    Atualização 01 exercício

--- modulo2/apir-labenusers/apis-labenusers/src/App.js
+++ modulo2/apir-labenusers/apis-labenusers/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = styled.button`
+width: 105px;
+height: 20px;
+`
+
+class App extends React.Component {
+  state = {
+    registro: [
+      {
+        id: "",
+        nome: "",
+        email: ""
+      }
+    ],
+    nomeBase: "",
+    emailBase: ""
+  }
+
+
+  updateNome = (e) => {
+    console.log("Nome foi atualizado");
+    this.setState({ nomeBase: e.target.value })
+  }
+
+  updateEmail = (e) => {
+    console.log("Email foi atualizado");
+    this.setState({ emailBase: e.target.value })
+  }
+
+  criaUsuario = (e) => {
+    console.log("Fui clicado");
+    const novoUsuario = {
+      nome: this.state.nomeBase,
+      email: this.state.emailBase
+    }
+    const novoArrayUsuario = [...this.state.registro, novoUsuario]
+    this.setState({ registro: novoArrayUsuario, nomeBase: "", emailBase: "" })
+  }
+
+  render() {
+
+    return (
+
+      <div>
+        <Button>Trocar de Tela</Button>
+        <input type="text" placeholder="Nome" onChange={this.updateNome} value={this.state.nomeBase} />
+        <input type="email" placeholder="Email" onChange={this.updateEmail} value={this.state.emailBase} />
+        <Button onClick={this.criaUsuario} >Criar Usuário</Button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
