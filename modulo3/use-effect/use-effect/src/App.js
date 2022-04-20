53b5f7d9a10f19b408f70e0ff3da7af7ede91272
William <william-freitas18@hotmail.com>
2022-04-20 18:25:04 -0300

    correção use-effect

--- modulo3/use-effect/use-effect/src/App.js
+++ modulo3/use-effect/use-effect/src/App.js
@@ -1,37 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PokeCard from "./components/PokeCard/PokeCard";
-import "./styles.css";
 
-export default function App() {
-  const [pokeName, setPokeName] = useState("")
-  const [pokeList, setPokelist] = useState([])
-
-  getPokemons = () => {
-    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151")
-      .then(response => {
-        // função que está setando no estado os 151 pokemons
-        setPokelist(pokeList = response.data.results);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+const App = () => {
+  const [pokeList, setPokeList] = useState([]);
+  const [pokeName, setPokeName] = useState("");
 
   useEffect(() => {
-    getPokemons()
-  }, [pokeList])
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then((res) => {
+        setPokeList(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-
-  changePokeName = event => {
-    setPokeName(pokeName = event.target.value);
+  const changePokeName = (event) => {
+    setPokeName(event.target.value);
   };
 
   return (
-    <div className="App">
+    <div>
       <select onChange={changePokeName}>
-        <option value={pokeName}>Nenhum</option>
-        {pokeList.map(pokemon => {
+        <option value={""}>Nenhum</option>
+        {pokeList.map((pokemon) => {
           return (
             <option key={pokemon.name} value={pokemon.name}>
               {pokemon.name}
@@ -42,4 +36,6 @@ export default function App() {
       {pokeName && <PokeCard pokemon={pokeName} />}
     </div>
   );
-}
+};
+
+export default App;
--- modulo3/use-effect/use-effect/src/components/PokeCard/PokeCard.js
+++ modulo3/use-effect/use-effect/src/components/PokeCard/PokeCard.js
@@ -1,32 +1,37 @@
-import React, { setState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-export default function PokeCard() {
-    const [pokemon, setPokemon] = setState({})
+const PokeCard = (props) => {
+  const [currentPokemon, setCurrentPokemon] = useState({});
 
-    useEffect(() => {
-        pegaPokemon()
-    }, [pokemon])
+  useEffect(() => {
+    pegaPokemon(props.pokemon);
+  }, [props.pokemon]);
 
-    pegaPokemon = pokeName => {
-        axios
-            .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
-            .then(response => {
-                setPokemon(pokemon = response.data);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    };
+  const pegaPokemon = (pokeName) => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+      .then((response) => {
+        setCurrentPokemon(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    return (
-        <div>
-            <p>{pokemon.name}</p>
-            <p>{pokemon.weight} Kg</p>
-            {pokemon.types && <p>{pokemon.types[0].type.name}</p>}
-            {pokemon.sprites && (
-                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-            )}
-        </div>
-    )
-}
+  return (
+    <div>
+      <p>{currentPokemon.name}</p>
+      <p>{currentPokemon.weight} Kg</p>
+      {currentPokemon.types && <p>{currentPokemon.types[0].type.name}</p>}
+      {currentPokemon.sprites && (
+        <img
+          src={currentPokemon.sprites.front_default}
+          alt={currentPokemon.name}
+        />
+      )}
+    </div>
+  );
+};
+
+export default PokeCard;
