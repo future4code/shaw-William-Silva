6670648c520676b915435e8731e26434fe2b74f7
William <william-freitas18@hotmail.com>
2022-06-08 22:00:42 -0300

    um pr aí

--- modulo4/knex-js/aula-knex-express-template/src/index.ts
+++ modulo4/knex-js/aula-knex-express-template/src/index.ts
@@ -12,7 +12,6 @@ const getActorById = async (id: string): Promise<any> => {
     return result[0][0]
 }
 
-
 // Assim a chamada funciona fora dos endpoints com .then()/.catch
 getActorById("001")
     .then(result => {
@@ -29,7 +28,7 @@ getActorById("001")
 
 
 // Ou então podemos chamá-la dentro de um endpoint
-app.get("/users/:id", async (req: Request, res: Response) => {
+app.get("/userid/:id", async (req: Request, res: Response) => {
     try {
         const id = req.params.id
 
@@ -41,3 +40,51 @@ app.get("/users/:id", async (req: Request, res: Response) => {
         res.status(500).send("Unexpected error")
     }
 }) // bata no http://localhost:3003/users/001 e veja o que acontece no terminal
+
+// EXERCICIO 1
+
+// a) O raw com o result [0] retorna um array com a informação requerida e o outro [0] retorna apenas as informações necessárias do objeto, ex: id, name, salary e etc...
+
+// b) 
+const searchActor = async (name: string): Promise<any> => {
+    const result = await connection.raw(`
+      SELECT * FROM Actor WHERE name = "${name}"
+    `)
+    return result[0]
+}
+
+app.get("/username/:name", async (req: Request, res: Response) => {
+    try {
+        const name = req.params.name
+
+        res.send(await searchActor(name))
+
+        res.end()
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(500).send("Unexpected error")
+    }
+})
+
+//c) 
+const countActors = async (gender: string): Promise<any> => {
+    const result = await connection.raw(`
+      SELECT COUNT(*) as count FROM Actor WHERE gender = "${gender}"
+    `);
+
+    const count = result[0][0].count;
+    return count;
+};
+
+app.get("/usergender/:gender", async (req: Request, res: Response) => {
+    try {
+        const gender = req.params.gender
+
+        res.send(await countActors(gender))
+
+        res.end()
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(500).send("Unexpected error")
+    }
+})
